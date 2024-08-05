0955f170818f53d965a3d21cfe319b0cdd6fc29b
Daniel Grbac Bravo <danielgrbacbravo@gmail.com>
2024-08-05 14:30:55 +0100

    feat: added both processing for with and without outliers

--- src/main.py
+++ src/main.py
@@ -97,6 +97,10 @@ def main():
   local_students, incoming_students = formatter.drop_irrelevant_columns(local_students, incoming_students)
   logging.info("Irrelevant columns dropped")
 
+    #adjust dates
+  current_date = datetime.now()
+  formatter.adjust_dataframe_dates(local_students, ['Availability', 'AvailabilityText'], current_date)
+  formatter.adjust_dataframe_dates(incoming_students, ['Arrival'], current_date)
 
   threshold: float = 2.0
 
@@ -108,29 +112,88 @@ def main():
   are_outliers: bool = any(incoming_outliers)
 
   if are_outliers:
+
     logging.warning("Outliers found in incoming students using a threshold of %i and a STD of %s", threshold, local_std)
     str_outlier = outlier_calculator.outliers_to_str(incoming_students, incoming_outliers)
     for i in str_outlier:
       #print in red color
       print(f"\033[91m{i}\033[00m")
 
+    # create dulicate copies of the incoming and local students
+    local_students_no_outliers: pd.DataFrame= local_students.copy()
+    incoming_students_no_outliers: pd.DataFrame = incoming_students.copy()
+
       #remove outliers
-    incoming_students = outlier_calculator.remove_outliers(incoming_students, incoming_outliers)
+    incoming_students_no_outliers = outlier_calculator.remove_outliers(
+      incoming_students_no_outliers
+      , incoming_outliers)
+
     logging.warning("Outliers removed from incoming students, manual intervention may be required.")
+
+       #print in red colo  # convert categories to numerical values
+    local_students_no_outliers, incoming_students_no_outliers= formatter.convert_categories_to_numerical(
+    local_students_no_outliers,
+    incoming_students_no_outliers,
+    hobbies)
+     # calulate capacities of the local students and number of  incoming students
+    base_local_capacity: int  = formatter.get_base_capacities(local_students_no_outliers)
+    base_incoming_necessity: int = formatter.get_base_necessity(incoming_students_no_outliers)
+
+
+    logging.info("Base capacities and necessities calculated")
+
+    logging.info("base local capacity: %s", base_local_capacity)
+    logging.info("base incoming necessity: %s", base_incoming_necessity)
+
+    if base_local_capacity < base_incoming_necessity:
+      logging.warning("The base local capacity is less than the base incoming necessity")
+      logging.warning("The algorithm may not be able to match all incoming students")
+
+
+     # compute the bounds for the different categories
+    config = configparser.ConfigParser()
+    config.read("config/config.ini")
+    normal_dict: Dict[str, Union[float,int]] = normalization_calculator.compute_normalization_values(
+      local_students_no_outliers,
+      incoming_students_no_outliers,
+      config,
+      hobbies,
+      faculty_distances)
+    logging.info("Normalization values computed")
+
+
+    for key, value in normal_dict.items():
+      logging.info("value for %s: %s", key, value)
+
+    distance_matrix: pd.DataFrame = distance_calculator.caculate_student_distances(
+    local_students_no_outliers,
+    incoming_students_no_outliers,
+    config,
+    normal_dict,
+    faculty_distances,
+    hobbies)
+
+    logging.info("Distance matrix computed")
+
+    logging.info("beggining the Kuhn-Munkres algorithm for building the matching matrix")
+    matching_matrix: pd.DataFrame = student_matcher.compute_optimal_pairs(distance_matrix, local_students_no_outliers, incoming_students_no_outliers, base_local_capacity, base_incoming_necessity)
+
+
+    print(matching_matrix)
+
 
   else:
     logging.info("No outliers found in incoming students using a threshold of %i and a STD of %s", threshold, local_std)
 
 
-    #print in red colo  # convert categories to numerical values
-  local_students, incoming_students = formatter.convert_categories_to_numerical(local_students, incoming_students, hobbies)
+  logging.info("computing matrix with outliers as well")
 
-  #adjust dates
-  current_date = datetime.now()
-  formatter.adjust_dataframe_dates(local_students, ['Availability', 'AvailabilityText'], current_date)
-  formatter.adjust_dataframe_dates(incoming_students, ['Arrival'], current_date)
-
-  # calulate capacities of the local students and number of  incoming students
+      #print in red colo  # convert categories to numerical values
+  local_students, incoming_students = formatter.convert_categories_to_numerical(
+  local_students,
+  incoming_students,
+  hobbies)
+    # calulate capacities of the local students and number of  incoming students
   base_local_capacity: int  = formatter.get_base_capacities(local_students)
   base_incoming_necessity: int = formatter.get_base_necessity(incoming_students)
 
@@ -145,26 +208,38 @@ def main():
     logging.warning("The algorithm may not be able to match all incoming students")
 
 
-
-  # compute the bounds for the different categories
+    # compute the bounds for the different categories
   config = configparser.ConfigParser()
   config.read("config/config.ini")
   normal_dict: Dict[str, Union[float,int]] = normalization_calculator.compute_normalization_values(
-    local_students, incoming_students, config, hobbies, faculty_distances)
+    local_students,
+    incoming_students,
+    config,
+    hobbies,
+    faculty_distances)
   logging.info("Normalization values computed")
 
 
   for key, value in normal_dict.items():
     logging.info("value for %s: %s", key, value)
 
-  distance_matrix: pd.DataFrame = distance_calculator.caculate_student_distances(local_students, incoming_students,config, normal_dict, faculty_distances,hobbies)
+  distance_matrix: pd.DataFrame = distance_calculator.caculate_student_distances(
+  local_students,
+  incoming_students,
+  config,
+  normal_dict,
+  faculty_distances,
+  hobbies)
+
   logging.info("Distance matrix computed")
 
   logging.info("beggining the Kuhn-Munkres algorithm for building the matching matrix")
   matching_matrix: pd.DataFrame = student_matcher.compute_optimal_pairs(distance_matrix, local_students, incoming_students, base_local_capacity, base_incoming_necessity)
 
-
   print(matching_matrix)
+
+
+
 
 
 if __name__ == '__main__':
